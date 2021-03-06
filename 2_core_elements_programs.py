9550fb9a931d05a167f67ddfdb1dae584703739a
jaywomack <womackjw@hotmail.com>
2021-03-05 21:09:18 -0800

    test for module 1

--- 2_core_elements_programs.py
+++ 2_core_elements_programs.py
@@ -96,3 +96,51 @@ for i in range(end + 1):
     count += i
 
 print(count)
+
+# check if is a perfect cube
+# # x = int(input('please input an integer'))
+# ans = 0
+# while ans ** 3 < x:
+#     ans = ans + 1
+# if ans **3 != x:
+#     print(str(x) + ' is not a perfect cube')
+# else:
+#     print('Cube root of ' + str(x) + ' is ' + str(ans) )
+
+# to make this code work with negative numbers as well abs and an if statement can be used to modify it
+x= int(input('please input an integer'))
+ans = 0
+while ans**3 < abs(x):
+    ans = ans + 1
+if ans**3 != abs(x):
+    print(str(x) + ' is not a perfect match')
+else:
+    if x < 0:
+        ans = - ans
+    print('Cube root of ' + str(x) + ' is ' + str(ans))
+
+
+# using a for loop to find a cube
+cube = 8
+for guess in range(abs(cube) + 1):
+    if guess**3  >= abs(cube):
+        break
+if guess**3 != abs(cube):
+    print(str(cube) + ' is not a perfect cube')
+else:
+    if cube < 0 :
+        guess = -guess
+    print('Cube root of ' + str(cube) + ' is ' +  str(guess))
+
+# Problem Set
+# Assume s is a string of lower case characters.
+
+# Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o',
+# and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
+s= 'asoidfnoawienoiwernf'
+vowels = 'aeiou'
+counter = 0
+for char in s:
+    if char in vowels:
+        counter +=1
+print(f'Number of vowels: {counter}')
